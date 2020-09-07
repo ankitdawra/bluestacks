f6e664dc7621f2facdf814b2cc0bc32189eeb315
Ankit Dawra <ankit.dawra@theteamie.com>
2020-09-07 16:51:45 +0530

    build

---
+++ build/precache-manifest.28b63aea860f6ec47b1c487b7a35feb0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f4cfcbf0c4a9b2fa09f1115352abc0a",
+    "url": "/bluestacks/index.html"
+  },
+  {
+    "revision": "b3ae76f6193fd814795a",
+    "url": "/bluestacks/static/css/2.2f73783b.chunk.css"
+  },
+  {
+    "revision": "ef7166ab3ce0492649c7",
+    "url": "/bluestacks/static/css/main.fb7e88d3.chunk.css"
+  },
+  {
+    "revision": "b3ae76f6193fd814795a",
+    "url": "/bluestacks/static/js/2.bb678cbc.chunk.js"
+  },
+  {
+    "revision": "94e8d68541a966facf9b5ad6c10f5a31",
+    "url": "/bluestacks/static/js/2.bb678cbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef7166ab3ce0492649c7",
+    "url": "/bluestacks/static/js/main.b0454d63.chunk.js"
+  },
+  {
+    "revision": "257844a18dbab871af74",
+    "url": "/bluestacks/static/js/runtime-main.3ae54d1a.js"
+  },
+  {
+    "revision": "1f4b57666ba11e9abc2cec5cb76828ff",
+    "url": "/bluestacks/static/media/logo.1f4b5766.png"
+  }
+]);
