5fd0bf9ac0435c403f3cc2ba1b4cb9ce7abe0b93
Ankit Dawra <ankit.dawra@theteamie.com>
2020-09-12 19:11:27 +0530

    Updates

--- precache-manifest.3625eaeb105eeb0a2f0a0b2aa483196f.js
+++ precache-manifest.3625eaeb105eeb0a2f0a0b2aa483196f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "aa726bd12ffda7cedaf4f871c36064c7",
+    "revision": "5683fca902ad4ad9b4eef14d9f555475",
     "url": "/bluestacks/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/bluestacks/static/css/2.2f73783b.chunk.css"
   },
   {
-    "revision": "acbdd5504de79660792c",
+    "revision": "9eb05df0df18b52f971c",
     "url": "/bluestacks/static/css/main.0993e88c.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/bluestacks/static/js/2.59b6d956.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "acbdd5504de79660792c",
-    "url": "/bluestacks/static/js/main.576d2e09.chunk.js"
+    "revision": "9eb05df0df18b52f971c",
+    "url": "/bluestacks/static/js/main.2c6cbcfa.chunk.js"
   },
   {
     "revision": "257844a18dbab871af74",
